5a456edc863f385d7358453595cf9efd82f180b9
vikasm <vikasm@woi-lt-224>
2018-03-19 18:28:49 +0530

    Added session

--- HIbernateTest/src/main/java/com/solane/dao/ImageDAO.java
+++ HIbernateTest/src/main/java/com/solane/dao/ImageDAO.java
@@ -2,10 +2,20 @@ package com.solane.dao;
 
 import javax.transaction.Transactional;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Transactional
 public class ImageDAO {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	private Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
 
 }
